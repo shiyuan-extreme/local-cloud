8a24529bbf553632f036940b2583046f6b915357
sychen <sychen@aerohive.com>
2020-09-17 22:22:47 +0800

    add file

---
+++ utils/create-local-valumes.sh
@@ -0,0 +1,10 @@
+DISK_MOUNT_POINT=${DISK_MOUNT_POINT:="/persistdata"}
+
+create_bind_mount(){
+	PV_BASE=${DISK_MOUNT_POINT}/$1
+	test -d $PV_BASE || mkdir -p $PV_BASE
+	PV=$(mktemp -d -p $PV_BASE data.XXXXXX)
+	mount --bind $PV $PV
+}
+
+create_bind_mount hm-elastic
