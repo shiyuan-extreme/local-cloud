26dc3784ff20fcb8748f82596ed08a20dd770f6b
sychen <sychen@aerohive.com>
2020-10-20 17:05:41 +0800

    update

--- utils/install_xapp.sh
+++ utils/install_xapp.sh
@@ -17,7 +17,7 @@ rancher_install(){
 		--set chart.version=${CHART_VERSION} \
 		--set chart.name=${CHART_NAME}   \
 		--set releaseName=${RELEASE_NAME} \
-        --set acctServer=${GDC_SERVER} \
+        --set xconf.acctServer=${GDC_SERVER} \
         --set xconf.loginUrl=${LOGIN_URL} \
         --set xconf.domainZone=${RDC_CUSTOMER_DOMAIN} \
         --set xconf.hmVersion=${XIQ_VERSION} \
@@ -29,7 +29,7 @@ rancher_install(){
 # deploy GCR pulling secret
 rancher kubectl -n ${NAMESPACE} apply -f https://raw.githubusercontent.com/shiyuan-extreme/local-cloud/master/miscs/gcr-pull-secret.yaml
 
-# create pvc for file server 
+# create pvc for file server
 rancher kubectl -n ${NAMESPACE} apply -f https://raw.githubusercontent.com/shiyuan-extreme/local-cloud/master/miscs/efs-fileserver-pvc.yaml
 
 # # install settings
@@ -53,9 +53,9 @@ rancher_install mx-relay "" 1.1.1
 # install middlewares
 rancher_install	hm-postgresql-ha
 rancher_install hm-ignite "" 3.1.1
-rancher_install ple-ignite 
-rancher_install	hm-rabbitmq 
-rancher_install	monitor-rabbitmq 
+rancher_install ple-ignite
+rancher_install	hm-rabbitmq
+rancher_install	monitor-rabbitmq
 rancher_install	ple-rabbitmq
 rancher_install	hm-elastic
 rancher_install	ple-elastic
@@ -75,24 +75,24 @@ is_schema_ready(){
 is_schema_ready
 
 # install applications
-rancher_install nginx-frontend  
-rancher_install nginx-backend "" 3.3.0 
-rancher_install nginx-static    
-rancher_install hmweb           
-rancher_install hmreport        
-rancher_install xapi            
-rancher_install afsweb          
-rancher_install scheduler       
-rancher_install idmauth         
-rancher_install idmlog          
-rancher_install idmcwp          
-rancher_install capwapmaster    
-rancher_install capwapserver capwapserver-0  
+rancher_install nginx-frontend
+rancher_install nginx-backend "" 3.3.0
+rancher_install nginx-static
+rancher_install hmweb
+rancher_install hmreport
+rancher_install xapi
+rancher_install afsweb
+rancher_install scheduler
+rancher_install idmauth
+rancher_install idmlog
+rancher_install idmcwp
+rancher_install capwapmaster
+rancher_install capwapserver capwapserver-0
 rancher_install capwapserver capwapserver-1
-rancher_install taskengine teall           
-rancher_install ple             
-rancher_install hacr            
-rancher_install wing-config     
-rancher_install wing-stats      
-rancher_install cloudcwp        
-rancher_install notify-engine      
+rancher_install taskengine teall
+rancher_install ple
+rancher_install hacr
+rancher_install wing-config
+rancher_install wing-stats
+rancher_install cloudcwp
+rancher_install notify-engine
